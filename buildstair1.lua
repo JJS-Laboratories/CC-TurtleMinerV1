1a7d7dc1d0127561cbdb2f5b333e6ae195d2196f
Jaja Steele <superjaja05@gmail.com>
2021-09-15 11:46:18 +0200

    Update buildstair1.lua

--- buildstair1.lua
+++ buildstair1.lua
@@ -24,6 +24,28 @@ function up()
     turtle.up()
 end
 
+function search()
+    print("Initalizing search for blocks")
+    slot1 = 1
+    done1 = false
+    repeat
+        print("Searching slot "..slot1)
+        if turtle.getItemDetail(slot1) ~= nil then
+            turtle.select(slot1)
+            turtle.transferTo(1)
+            turtle.select(1)
+            break
+        else
+            slot1 = slot1+1
+        end
+        if slot1 == 16 and turtle.getItemDetail(16) == nil then
+            os.sleep(2)
+            textutils.slowPrint("Not found.. rebooting")
+            os.reboot()
+        end
+    until done1 == true
+end
+
 
 turtle.place()
 turtle.placeUp()
@@ -32,6 +54,9 @@ turtle.forward()
 turtle.up()
 turtle.placeUp()
 repeat
+    if turtle.getItemDetail() == nil then
+        search()
+    end
     tl()
     fw()
     up()
